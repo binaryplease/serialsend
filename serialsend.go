7b5ffa6af5aab27612f218f27b1efe86c202157a
binaryplease <binaryplease@gmail.com>
2017-06-29 02:26:30 +0200

    Switched to mac-compatibe serial lib

--- serialsend.go
+++ serialsend.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"log"
-
-	"github.com/tarm/serial"
+	"fmt"
+	"github.com/jacobsa/go-serial/serial"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"log"
 )
 
+// Read command-line arguments
 var (
 	debug = kingpin.Flag("debug", "Enable debug mode. Also prints data to the console ").Short('v').Bool()
 	sPort = kingpin.Flag("port", "Serialport to use e.g. /dev/ttyUSB1").Short('p').Required().String()
-	sBaud = kingpin.Flag("baud", "Baudrate to send with").Default("115200").Short('b').Int()
+	sBaud = kingpin.Flag("baud", "Baudrate to send with").Default("115200").Short('b').Uint()
 	sData = kingpin.Arg("data", "Data to send to the port").Required().String()
 )
 
@@ -18,16 +19,32 @@ func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
 
-	c := &serial.Config{Name: *sPort, Baud: *sBaud}
-	s, err := serial.OpenPort(c)
-	if err != nil {
-		log.Fatal(err)
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName:        *sPort,
+		BaudRate:        *sBaud,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
 	}
 
-	n, err := s.Write([]byte(*sData))
+	// Open the port.
+	port, err := serial.Open(options)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("serial.Open: %v", err)
 	}
 
-	log.Printf("%q", []byte(*sData)[:n])
+	// Make sure to close it later.
+	defer port.Close()
+
+	// Write bytes to the port.
+	n, err := port.Write([]byte(*sData))
+	if err != nil {
+		log.Fatalf("port.Write: %v", err)
+	}
+
+	if *debug {
+		fmt.Println("Wrote", n, "bytes.")
+		fmt.Println(*sData)
+	}
 }
